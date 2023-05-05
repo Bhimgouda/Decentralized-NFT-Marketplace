68786981adf06fb2151725ff62b990b19f112840
Bhimgouda <satvikp508@gmail.com>
2023-05-05 15:33:08 +0530

    refactored & added moralis frontend

--- backend-smart-contracts/contracts/test/BasicNFT.sol
+++ backend-smart-contracts/contracts/test/BasicNFT.sol
@@ -3,25 +3,28 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 pragma solidity ^0.8.8;
 
-contract BasicNFT is ERC721{
-
+contract BasicNFT is ERC721 {
     uint256 private s_tokenCounter;
     string public constant TOKEN_URI =
         "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
 
+    event DogMinted(uint256 indexed tokenId);
+
     constructor() ERC721("RageDog", "RGD") {}
 
-    function mintNft() public returns(uint256){
+    function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
+        emit DogMinted(s_tokenCounter);
         s_tokenCounter++;
-        return s_tokenCounter;
     }
 
-    function tokenURI(uint256 /*tokenId */) public pure override returns(string memory){
+    function tokenURI(
+        uint256 /*tokenId */
+    ) public pure override returns (string memory) {
         return TOKEN_URI;
     }
 
-    function getTokenCounter() public view returns (uint256){
+    function getTokenCounter() public view returns (uint256) {
         return s_tokenCounter;
     }
 }
---
+++ backend-smart-contracts/scripts/mint-and-list-nft.js
@@ -0,0 +1,29 @@
+const {network, ethers} = require("hardhat");
+
+const PRICE = ethers.utils.parseEther("0.1")
+
+async function mintAndList() {
+    const basicNft = await ethers.getContract("BasicNFT") 
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+
+    const mintTx = await basicNft.mintNft()
+    const mintTxReceipt = await mintTx.wait(1)
+    const tokenId = mintTxReceipt.events[0].args.tokenId
+
+    console.log("Giving approval.....");
+    const approveTx = await basicNft.approve(nftMarketplace.address, tokenId)
+    await approveTx.wait(1)
+
+    console.log("Listing Nft...")
+    const listingTx = await nftMarketplace.listItem(basicNft.address, tokenId, PRICE)
+    await listingTx.wait(1)
+
+    console.log("NFT is listed!")
+}
+
+mintAndList()
+    .then(()=>process.exit(0))
+    .catch((error) => {
+        console.log(error)
+        process.exit(1)
+    })
